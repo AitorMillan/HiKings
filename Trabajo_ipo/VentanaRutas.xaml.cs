1cf2f64e8e16a70ea0a6b3c81cbe850a917e5cfa
Alzhe1 <aitormillantrujillo@gmail.com>
2022-12-28 17:32:06 +0100

    Creadas ventanas rutas y guias

---
+++ Trabajo_ipo/VentanaRutas.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Trabajo_ipo
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaRutas.xaml
+    /// </summary>
+    public partial class VentanaRutas : Window
+    {
+        public VentanaRutas()
+        {
+            InitializeComponent();
+        }
+    }
+}
